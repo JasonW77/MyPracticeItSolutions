e0b312942173d5785ecc755b267d6484f9563fd7
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-12-12 20:25:51 -0700

    Create 1_15_printlnSlashes.java

---
+++ 1_15_printlnSlashes.java
@@ -0,0 +1,13 @@
+/*
+Write a println statement that produces the following output:
+
+/ \ // \\ /// \\\
+*/
+
+
+public class PrintInSlashes {
+  public static void main(String[] args) {
+    System.out.println("/ \\ // \\\\ /// \\\\\\");
+    
+  }
+}  
