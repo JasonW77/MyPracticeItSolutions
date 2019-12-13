efbca40fd33d98832649b6d0ce5989dc7a536b81
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-12-13 00:00:34 -0700

    Create 1_13_StarFigures.java

---
+++ 1_13_StarFigures.java
@@ -0,0 +1,52 @@
+/*
+Write a complete Java program in a class named StarFigures that generates the following output. Use static methods to show structure and eliminate redundancy in your solution.
+
+*****
+*****
+ * *
+  *
+ * *
+
+*****
+*****
+ * *
+  *
+ * *
+*****
+*****
+
+  *
+  *
+  *
+*****
+*****
+ * *
+  *
+ * *
+ */
+ 
+ 
+ public class StarFigures{
+    public static void main(String[] args){
+        method1();
+        method2();
+        System.out.println();
+        method1();
+        method2();
+        method1();
+        System.out.println();
+        method3();
+        method1();
+        method2();
+    }
+    public static void method1(){
+        System.out.println("*****\n*****");
+    }
+    public static void method2(){
+        System.out.println(" * *\n  *\n * * ");
+               
+    }
+    public static void method3(){
+        System.out.println("  *\n  *\n  *");
+    }
+}
